8c3b342d57f0567e3c75fbea681328ce229e3adf
Azlan <azlan@kalohq.com>
2018-08-08 08:38:10 +0100

    32 Reacting to incoming events (in SW)
    Signed-off-by: Azlan <azlan@kalohq.com>

--- course-project/public/sw.js
+++ course-project/public/sw.js
@@ -1,2 +1,12 @@
 // Note scope of the service worker is defined by its location in the 
-// folder structure
+// folder structure. Scope can be further limited when the service worker is
+// registered if required.
+
+self.addEventListener('install', function(event) {
+  console.log('[Service Worker]: Installing service worker...', event);
+});
+
+self.addEventListener('activate', function(event) {
+  console.log('[Service Worker]: Activating service worker...', event);
+  return self.clients.claim();
+});
