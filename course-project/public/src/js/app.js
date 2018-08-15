13d4122358a6faed8d9cb871811347cc6e9cfca4
Azlan <azlan@kalohq.com>
2018-08-15 08:03:21 +0100

    44 Async code in Javascript
    Signed-off-by: Azlan <azlan@kalohq.com>

--- course-project/public/src/js/app.js
+++ course-project/public/src/js/app.js
@@ -15,3 +15,9 @@ window.addEventListener('beforeinstallprompt', function(event) {
   deferredPrompt = event;
   return false;
 });
+
+setTimeout(function() {
+  console.log('This is executed once the timer is done');
+}, 3000);
+
+console.log('This is executed right after setTimeout()');
