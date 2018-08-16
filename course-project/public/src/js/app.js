3cd6d6cb03ed2b1e231bdaf031831f32c4aea3eb
Azlan <azlan@kalohq.com>
2018-08-16 08:22:30 +0100

    49 Sending POST requests via Fetch
    Signed-off-by: Azlan <azlan@kalohq.com>

--- course-project/public/src/js/app.js
+++ course-project/public/src/js/app.js
@@ -16,15 +16,33 @@ window.addEventListener('beforeinstallprompt', function(event) {
   return false;
 });
 
-var promise = new Promise(function(resolve, reject) {
-  setTimeout(function() {
+// var promise = new Promise(function(resolve, reject) {
+//   setTimeout(function() {
     // resolve('This is executed once the timer is done');
-    reject({code: 500, message: 'An error occurred'});
+    // reject({code: 500, message: 'An error occurred'});
     // console.log('This is executed once the timer is done');
-  }, 3000);
-});
+//   }, 3000);
+// });
 
 fetch('https://httpbin.org/ip')
+.then(function(response){
+  console.log(response);
+  response.json()
+    .then(function(data) {
+      console.log(data);
+    }).catch(function(err) {
+      console.log(err);
+    });
+});
+
+fetch('https://httpbin.org/post', {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json'
+  },
+  body: JSON.stringify({message: 'Does this work'})
+})
 .then(function(response){
   console.log(response);
   response.json()
@@ -43,12 +61,12 @@ fetch('https://httpbin.org/ip')
 //   console.log(newText);
 // });
 
-promise.then(function(text) {
-  return text;
-}).then(function(newText) {
-  console.log(newText);
-}).catch(function(err) {
-  console.log(err.code, err.message);
-});
+// promise.then(function(text) {
+//   return text;
+// }).then(function(newText) {
+//   console.log(newText);
+// }).catch(function(err) {
+//   console.log(err.code, err.message);
+// });
 
-console.log('This is executed right after setTimeout()');
+// console.log('This is executed right after setTimeout()');
