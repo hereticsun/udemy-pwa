e14ccf90712a43d1a14f0aa82c8ad46285525082
Azlan <azlan@kalohq.com>
2018-08-21 08:27:41 +0100

    Completed assignment
    Signed-off-by: Azlan <azlan@kalohq.com>

--- problem--promises-fetch-assignment/public/src/js/app.js
+++ problem--promises-fetch-assignment/public/src/js/app.js
@@ -4,16 +4,26 @@ var output = document.querySelector('#output');
 
 button.addEventListener('click', function() {
   // Create a new Promise here and use setTimeout inside the function you pass to the constructor
-
-  setTimeout(function() { // <- Store this INSIDE the Promise you created!
-    // Resolve the following URL: https://swapi.co/api/people/1
-  }, 3000);
+  var promise = new Promise(function(resolve, reject) {
+    setTimeout(function() { // <- Store this INSIDE the Promise you created!
+      // Resolve the following URL: https://swapi.co/api/people/1
+      resolve('https://swapi.co/api/people/1');
+    }, 3000);
+  })
+    .then(function(url) {
+      // Handle the response of the fetch() call and extract the JSON data, return that
+      // and handle it in yet another then() block
+      return fetch(url);
+    })
+    .then(function(response) {
+      response.json()
+    })
+    .then(function(data) {
+      output.textContent = data.name;
+    });
 
   // Handle the Promise "response" (=> the value you resolved) and return a fetch()
   // call to the value (= URL) you resolved (use a GET request)
-
-  // Handle the response of the fetch() call and extract the JSON data, return that
-  // and handle it in yet another then() block
 
   // Finally, output the "name" property of the data you got back (e.g. data.name) inside
   // the "output" element (see variables at top of the file)
@@ -23,6 +33,36 @@ button.addEventListener('click', function() {
   // Send any data of your choice, make sure to access it correctly when outputting it
   // Example: If you send {person: {name: 'Max', age: 28}}, you access data.json.person.name
   // to output the name (assuming your parsed JSON is stored in "data")
+  var promise2 = new Promise(function(resolve, reject) {
+    setTimeout(function() { // <- Store this INSIDE the Promise you created!
+      // Resolve the following URL: https://swapi.co/api/people/1
+      resolve('https://httpbin.org/put');
+      reject({code: 500, message: 'An error occurred'});
+    }, 3000);
+  })
+    .then(function(url) {
+      // Handle the response of the fetch() call and extract the JSON data, return that
+      // and handle it in yet another then() block
+      return fetch(url, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+        },
+        body: JSON.stringify({person: {name: 'Max', age: 28}})
+      });
+    })
+    .then(function(response) {
+      response.json()
+    }, function(str){
+      console.log(err);
+    })
+    .then(function(data) {
+      output.textContent = data.json.person.name;
+    })
+    .catch(function(err) {
+      console.log(err);
+    });
 
   // To finish the assignment, add an error to URL and add handle the error both as
   // a second argument to then() as well as via the alternative taught in the module
