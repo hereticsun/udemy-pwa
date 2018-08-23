99f01969e6d8a541b9cddb04e0b7beb6334f0578
Azlan <azlan@kalohq.com>
2018-08-23 08:06:10 +0100

    Clean up
    Signed-off-by: Azlan <azlan@kalohq.com>

--- course-project/public/sw.js
+++ course-project/public/sw.js
@@ -1,17 +1,13 @@
-// Note scope of the service worker is defined by its location in the 
-// folder structure. Scope can be further limited when the service worker is
-// registered if required.
 
 self.addEventListener('install', function(event) {
-  console.log('[Service Worker]: Installing service worker...', event);
+  console.log('[Service Worker] Installing Service Worker ...', event);
 });
 
 self.addEventListener('activate', function(event) {
-  console.log('[Service Worker]: Activating service worker...', event);
+  console.log('[Service Worker] Activating Service Worker ....', event);
   return self.clients.claim();
 });
 
 self.addEventListener('fetch', function(event) {
-  console.log('[Service Worker]: Fetching something...', event);
   event.respondWith(fetch(event.request));
 });
