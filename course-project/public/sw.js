8d4a125c355f21a29effba169ea343638249a57c
Azlan <azlan@kalohq.com>
2018-09-14 08:42:49 +0100

    77 Providing an offline fallback page

--- course-project/public/sw.js
+++ course-project/public/sw.js
@@ -1,4 +1,4 @@
-var CACHE_STATIC_NAME = "static-v8";
+var CACHE_STATIC_NAME = "static-v10";
 var CACHE_DYNAMIC_NAME = "dynamic-v2";
 
 self.addEventListener("install", function(event) {
@@ -9,6 +9,7 @@ self.addEventListener("install", function(event) {
       cache.addAll([
         "/",
         "/index.html",
+        "/offline.html",
         "/src/js/app.js",
         "/src/js/feed.js",
         "/src/js/promise.js",
@@ -55,7 +56,11 @@ self.addEventListener("fetch", function(event) {
               return res;
             });
           })
-          .catch(function(err) {});
+          .catch(function(err) {
+            return caches.open(CACHE_STATIC_NAME).then(function(cache) {
+              return cache.match("/offline.html");
+            });
+          });
       }
     })
   );
