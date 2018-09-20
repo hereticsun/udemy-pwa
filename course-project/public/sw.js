c396030451bbfddaf0972455c1b688d21da892fe
Azlan <azlan@kalohq.com>
2018-09-20 08:45:22 +0100

    86 Applying cache only

--- course-project/public/sw.js
+++ course-project/public/sw.js
@@ -1,27 +1,28 @@
 var CACHE_STATIC_NAME = "static-v13";
 var CACHE_DYNAMIC_NAME = "dynamic-v2";
+var STATIC_FILES =  [
+  "/",
+  "/index.html",
+  "/offline.html",
+  "/src/js/app.js",
+  "/src/js/feed.js",
+  "/src/js/promise.js",
+  "/src/js/fetch.js",
+  "/src/js/material.min.js",
+  "/src/css/app.css",
+  "/src/css/feed.css",
+  "/src/images/main-image.jpg",
+  "https://fonts.googleapis.com/css?family=Roboto:400,700",
+  "https://fonts.googleapis.com/icon?family=Material+Icons",
+  "https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css"
+];
 
 self.addEventListener("install", function(event) {
   console.log("[Service Worker] Installing Service Worker ...", event);
   event.waitUntil(
     caches.open(CACHE_STATIC_NAME).then(function(cache) {
       console.log("[Service Worker] Precaching App Shell");
-      cache.addAll([
-        "/",
-        "/index.html",
-        "/offline.html",
-        "/src/js/app.js",
-        "/src/js/feed.js",
-        "/src/js/promise.js",
-        "/src/js/fetch.js",
-        "/src/js/material.min.js",
-        "/src/css/app.css",
-        "/src/css/feed.css",
-        "/src/images/main-image.jpg",
-        "https://fonts.googleapis.com/css?family=Roboto:400,700",
-        "https://fonts.googleapis.com/icon?family=Material+Icons",
-        "https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css"
-      ]);
+      cache.addAll(STATIC_FILES);
     })
   );
 });
@@ -62,6 +63,10 @@ self.addEventListener("fetch", function(event) {
       caches.match(event.request).then(function(response) {
         if (response) {
           return response;
+        } else if (new RegExp('\\b' + STATIC_FILES.join('\\b|\\b') + '\\b').test(event.request.url)) {
+          event.respondWith(
+            return response;
+          );
         } else {
           return fetch(event.request)
             .then(function(res) {
